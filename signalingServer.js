181355ba39221c2ead1e1ef7a1e755328434d247
HDhuangdi <1013697816@qq.com>
2022-09-07 00:18:45 +0800

    fix: 一些问题

--- signalingServer.js
+++ signalingServer.js
@@ -17,15 +17,31 @@ const io = new Server(server, { cors: true,secure: true });
 
 io.on("connection", (socket) => {
   socket.on("icecandidateP1", (e) => {
+    console.log("icecandidateP1");
     socket.broadcast.emit("icecandidateP1", e);
   });
   socket.on("icecandidateP2", (e) => {
+    console.log("icecandidateP2");
     socket.broadcast.emit("icecandidateP2", e);
   });
   socket.on("offer", (e) => {
+    console.log("offer");
     socket.broadcast.emit("offer", e);
   });
   socket.on("answer", (e) => {
+    console.log("answer");
     socket.broadcast.emit("answer", e);
+  });
+  socket.on("p1OnLine", (e) => {
+    console.log("p1OnLine");
+    socket.broadcast.emit("p1OnLine", e);
+  });
+  socket.on("p2OnLine", (e) => {
+    console.log("p2OnLine");
+    socket.broadcast.emit("p2OnLine", e);
+  });
+  socket.on("p2Done", (e) => {
+    console.log("p2Done");
+    socket.broadcast.emit("p2Done", e);
   });
 });
